a6d87137039518054ebf9393b702e874baf2156a
igarashi339 <igarashi339@gmail.com>
2021-07-24 11:42:28 +0900

    平均待ち時間を返却するようにAPIを改修

--- api/src/DeliverWaitTime.js
+++ api/src/DeliverWaitTime.js
@@ -1,13 +1,19 @@
-function doGet(e) {
-    if (e.parameter.mode == "latest") {
-        return getLatestWaitTime()
-    } else {
-      return ContentService.createTextOutput("error: parameter is invalid.");
-    }
+function doGet() {
+  const latestWaitTimeDict = getLatestWaitTime()
+  const meanWaitTimeDict = getMeanWaitTime()
+  let allInfoDict = {}
+  for (let key in meanWaitTimeDict) {
+    allInfoDict[key] = latestWaitTimeDict[key] + "," + meanWaitTimeDict[key]
+  }
+
+  var output = ContentService.createTextOutput()
+  output.setMimeType(ContentService.MimeType.JSON)
+  output.setContent(JSON.stringify(allInfoDict))
+  return output
 }
 
 /**
- * 現時点での最新の待ち時間情報を取得して返す。
+ * 現時点での最新の待ち時間情報をスプレッドシートから取得する。
  */
 function getLatestWaitTime() {
     const sheetId = "1GvymAJ57Il1dSbPGVeGv4PDwi6ymDs_MHfHDbosQFpc"
@@ -27,9 +33,25 @@ function getLatestWaitTime() {
     const fixedTime = Utilities.formatDate(new Date(waitTimeDict['timestamp']), 'Asia/Tokyo', 'yyyy/MM/dd HH:mm');
     waitTimeDict['timestamp'] = fixedTime
 
-    var output = ContentService.createTextOutput()
-    output.setMimeType(ContentService.MimeType.JSON)
-    output.setContent(JSON.stringify(waitTimeDict))
+    return waitTimeDict
+}
 
-    return output
+/**
+ * 現時点での平均待ち時間をスプレッドシートから取得する。
+ */
+function getMeanWaitTime() {
+  const sheetId = "1GvymAJ57Il1dSbPGVeGv4PDwi6ymDs_MHfHDbosQFpc"
+  const sheetName = "mean_wait_time"
+  var sheet = SpreadsheetApp.openById(sheetId).getSheetByName(sheetName)
+
+  // スプレッドシートから情報を取得する
+  const colNum = 29
+  const key = sheet.getRange(1, 1, 1, colNum).getValues()[0]
+  const data = sheet.getRange(sheet.getLastRow(), 1, 1, colNum).getValues()[0]
+  let meanWaitTimeDict = {}
+  for (let i = 0; i < colNum; ++i) {
+    meanWaitTimeDict[key[i]] = data[i]
+  }
+
+  return meanWaitTimeDict
 }
